78836be4c6793335ab2dd21eac9a1cdfe31ae2f1
Shavit Cohen <shavit@tikalk.com>
2014-12-11 20:52:34 +0200

    step 2 - list ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,12 @@
   })
 
   function drawList(data){
+      var ul = d3.select("#RegularList").append("ul");
+      var li = ul.select("li")
+          .data(data.children)
+          .enter()
+          .append("li");
+      li.text(function(d){return d.name + " - " + d.stargazers_count;})
   }
 
   function drawTable(data){
