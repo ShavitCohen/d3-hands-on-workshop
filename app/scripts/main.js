f08a40c5a29b53c000ee7c9288605cbf370bb909
Shavit Cohen <shavit@tikalk.com>
2014-12-12 15:57:41 +0200

    creating table

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,15 +9,38 @@
   })
 
   function drawList(data){
-      var ul = d3.select("#RegularList").append("ul");
-      var li = ul.select("li")
-          .data(data.children)
+      d3.selectAll("#RegularList").append("ol")
+        .selectAll("li")
+        .data(data.children)
           .enter()
-          .append("li");
-      li.text(function(d){return d.name + " - " + d.stargazers_count;})
+          .append("li")
+          .text(function(d){return d.name + " - " + d.stargazers_count;});
+
   }
 
   function drawTable(data){
+      var table = d3.selectAll("#RegularTable")
+          .append("table")
+          .classed({"table":true, "table-striped":true});
+      var thTr = table.append("thead").
+          append("tr");
+
+      thTr.append("th").text("#");
+      thTr.append("th").text("Project Name");
+      thTr.append("th").text("Author");
+      thTr.append("th").text("Stars");
+
+      var tdTr = table.append("tbody")
+          .selectAll("tr.data")
+          .data(data.children)
+          .enter()
+          .append("tr")
+          .classed({"data":true});
+
+      tdTr.append("td").text(function(d,i){return (i+1) + "."});
+      tdTr.append("td").text(function(d){return d.name});
+      tdTr.append("td").text(function(d){return d.user});
+      tdTr.append("td").text(function(d){return d.stargazers_count});
   }
 
   function drawPie(data){
